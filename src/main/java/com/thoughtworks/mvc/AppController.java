d5db653263744a170924f7677a0fda8ccb41cec9
Xue Qian <xueqian0321@126.com>
2014-11-09 11:29:17 +0800

    rename AppController.java

--- src/main/java/com/thoughtworks/mvc/AppController.java
+++ src/main/java/com/thoughtworks/mvc/AppController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/")
-public class HelloController {
+public class AppController {
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
